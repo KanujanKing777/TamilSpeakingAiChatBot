46f5039eb073835573cc5c1e88aea910c9b2455f
KanujanKing777 <piragadeesh25@gmail.com>
2024-02-21 05:30:35 +0530

    ad

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -14,9 +14,10 @@ def webhook():
             data = request.get_json()
             user_message = data["message"]
             user_message = user_message + ' '
+            user_message.lower()
             # Debugging: Print received data
             hi_list = ['hi ', 'hello ', 'vanakkam ', 'good morning ', 'good evening ', 'good afternoon ']
-            hi_list_gf = ['hi', 'hello', 'vanakkam']
+            hi_list_gf = ['Hi', 'Hello', 'Vanakkam']
             caller_list = ['da', 'dear', 'darling']
             how_list = ['how are you ', 'epdi irukureenga ', 'vaazhkai epdi pothu ']
             how_list_gf = ['I am fine bcz of u', 'I am in luv with u', 'I will kiss you']
@@ -24,13 +25,18 @@ def webhook():
             bye_list_gf = ['bye da', 'bye da my luv']
             casual = ['love u darling', 'luv u forever', 'u r my favourite',
                       'I luv u for infinity']
+            nouse = ['I ', 'am ', 'is ', 'was ', 'were ', 'you ', 'u ']
+
+            nuts = ['oh ', 'ohh ', 'mm ', 'mmm ', 'ah ', 'ahh ']
             
             if any(word in user_message for word in hi_list):
                 bot_reply = random.choice(hi_list_gf) + " " + random.choice(caller_list)
             elif any(word in user_message for word in how_list):
-                 bot_reply = random.choice(how_list_gf) 
+                bot_reply = random.choice(how_list_gf) 
             elif any(word in user_message for word in bye_list):
-                 bot_reply = random.choice(bye_list_gf) 
+                bot_reply = random.choice(bye_list_gf) 
+            elif any(word in user_message for word in nuts):
+                bot_reply = random.choice(nuts) 
             else:
                 bot_reply = random.choice(casual)
 
